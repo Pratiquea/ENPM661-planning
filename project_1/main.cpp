e99e70ce52bdd5ebaa5532ade26f40cc7903d7f1
Pratiquea <prateekarorav2@gmail.com>
2019-02-24 21:12:31 -0500

    added comparator and started solver code

--- project_1/main.cpp
+++ project_1/main.cpp
@@ -25,23 +25,8 @@ struct Node
 };
 
 
-// Function to print dim x dim matrix
-void print_puzzle(int map[dim][dim])
-{
-	cout<<endl;
-	for(i=0 ; i<= dim-1 ; i++)
-	{
-		for(j=0 ; j<= dim-1 ; j++)
-		{
-			cout<<map[i][j];
-		}
-		cout<<endl;
-	}
-}
-
-
 // Function to create/allocate a new node
-Node* new_node(int map[dim][dim], int x, int y, int move_to_x, int move_to_y, int accumulated_cost, Node* parent)
+Node* create_new_node(int map[dim][dim], int x, int y, int move_to_x, int move_to_y, int accumulated_cost, Node* parent)
 {
 	Node* node = new Node;
 	
@@ -68,7 +53,86 @@ Node* new_node(int map[dim][dim], int x, int y, int move_to_x, int move_to_y, in
 }
 
 
-// row and colomn values for bottom, left, top, right repectively
+// In order to connect nodes we need the following comparator
+struct compare_nodes
+{
+	bool operator()(const Node* a, Node* b) const
+	{
+		total_cost_a = a->cost_to_come + a->accumulated_cost;
+		total_cost_b = b->cost_to_come + b->accumulated_cost;
+
+		return(total_cost_a > total_cost_b)
+	}
+};
+
+
+// Function to print dim x dim matrix
+void print_puzzle(int map[dim][dim])
+{
+	cout<<endl;
+	for(i=0 ; i<= dim-1 ; i++)
+	{
+		for(j=0 ; j<= dim-1 ; j++)
+		{
+			cout<<map[i][j];
+		}
+		cout<<endl;
+	}
+}
+
+
+// row and colomn move values for bottom, left, top, right repectively
+int row[] = {1, 0, -1, 0};
+int col[] = {0, -1, 0, 1};
+
+//Function to calculate the cost to come or number of misplaced tiles i.e. number of tiles(except the blank one) not in their goal position
+int calculate_cost_to_come(int init_map[dim][dim], int goal_map[dim][dim])
+{
+	int cost = 0;
+	for(i=0 ; i<=dim-1 ; i++)
+	{
+		for(j=0 ; j<=dim-1 ; j++)
+		{
+			if(goal_map[i][j] && goal_map[i][j] != init_map[i][j])
+				cost++;
+		}
+	}	
+	return cost;
+}
+
+// Function to check validity of a position(coordinate)
+int is_valid(int x, int y)
+{
+	return(x>=0 && x<dim && y>=0 && y<dim);	
+}
+
+
+//Print the solution i.e the path from goal node to root node
+void print_solution(Node* root)
+{
+	if(root == NULL)
+		return;
+
+	// recursively call the function untill the first node
+	print_solution(root->parent);
+	print_puzzle(root->map);
+
+	cout<<endl;
+}
+
+
+//Puzzle solver code
+void solve(int init_state[dim][dim],int goal_state[dim][dim],int x, int y)
+{
+	// Building a queue structure to store active nodes of search tree
+	node_queue<Node*, std::vector<Node*>, compare_nodes> Q;
+
+	// create a root node and calculate its cost
+	Node* root = create_new_node(init_state,x,y,x,y,0,NUL);
+	//cost of this node is number of nodes 
+	root->cost_to_come = calculate_cost_to_come(init_state, goal_state);
+}
+
 
 
 
